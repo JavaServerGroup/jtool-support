1b3140e886ed2e438feb8adbb6f62e75b957eb4b
jiale.chan@gmail.com <jiale.chan@gmail.com>
2016-05-04 16:10:16 +0800

    jtool-support 将日志跟踪的功能移到LogHelper

--- src/main/java/com/jtool/support/log/LogFilter.java
+++ src/main/java/com/jtool/support/log/LogFilter.java
@@ -1,25 +1,20 @@
 package com.jtool.support.log;
 
-import org.slf4j.MDC;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 public class LogFilter implements Filter {
 
-    public static final String JTOOL_LOG_ID = "_logId";
-    public static final String JTOOL_LOG_USER_ID = "_logUserId";
-
     public void init(FilterConfig config) throws ServletException {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws java.io.IOException, ServletException {
-        String logId = ((HttpServletRequest)request).getHeader(JTOOL_LOG_ID);
+        String logId = ((HttpServletRequest)request).getHeader(LogHelper.JTOOL_LOG_ID);
         if(logId == null) {
             logId = UUID.randomUUID().toString();
         }
-        MDC.put(JTOOL_LOG_ID, logId);
+        LogHelper.setLogId(logId);
         chain.doFilter(request,response);
     }
 
--- src/main/java/com/jtool/support/log/LogHelper.java
+++ src/main/java/com/jtool/support/log/LogHelper.java
@@ -5,10 +5,38 @@ import org.slf4j.MDC;
 import javax.servlet.http.HttpServletResponse;
 
 public class LogHelper {
+
+    public static final String JTOOL_LOG_ID = "_logId";
+    public static final String JTOOL_LOG_USER_ID = "_logUserId";
+
     public static void addLogIdToHeader(HttpServletResponse httpServletResponse) {
-        String logId = MDC.get(LogFilter.JTOOL_LOG_ID);
-        if(logId != null) {
-            httpServletResponse.setHeader(LogFilter.JTOOL_LOG_ID, MDC.get(LogFilter.JTOOL_LOG_ID));
+        String logId = MDC.get(JTOOL_LOG_ID);
+        if (logId != null) {
+            httpServletResponse.setHeader(JTOOL_LOG_ID, MDC.get(JTOOL_LOG_ID));
         }
+    }
+
+    public static void setLogId(String logId) {
+        MDC.put(JTOOL_LOG_ID, logId);
+    }
+
+    public static void setLogUserId(String userId) {
+        MDC.put(JTOOL_LOG_USER_ID, userId);
+    }
+
+    public static void removeLogId(String logId) {
+        MDC.remove(JTOOL_LOG_ID);
+    }
+
+    public static void removeLogUserId(String userId) {
+        MDC.remove(JTOOL_LOG_USER_ID);
+    }
+
+    public static String getLogId() {
+        return MDC.get(JTOOL_LOG_ID);
+    }
+
+    public static String getLogUserId() {
+        return MDC.get(JTOOL_LOG_USER_ID);
     }
 }
